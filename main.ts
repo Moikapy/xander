e644ff95ef9f42cc39bccce03a08027f2cfc1092
Moikapy <warrenckhan@gmail.com>
2024-10-07 23:41:23 -0400

    initialize Dockerfiles+ compose for prod and development with npm scripts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-import { server } from "./server.ts";
+import { server } from "./server";
 
 async function main() {
   // Start The Server
--- server.ts
+++ server.ts
@@ -5,19 +5,23 @@ import bodyParser from "body-parser";
 
 export async function server() {
   try {
+    console.log("Starting Systems...")
     //init express
     const app = express();
-
+    console.log("Attaching Middleware");
+    
     app.use(bodyParser.json());
-
+    console.log("Initializng Routes");
+    
     await init_routes(app);
 
     // Start server and listen to PORT
     app.listen(process.env.PORT || 3000, () => {
-      console.log(`Server listening on port: ${process.env.PORT || 3000}`);
+      console.log(`Online at: http://localhost:${process.env.PORT || 3000}`);
     });
   } catch (error) {
     console.log("Error Message:", error.message);
     //ADD ERROR LOGGING FOR SERVER
   }
 }
+

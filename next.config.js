b732f4e13df05a13e86a54725f0d2cb211026a99
Warren <moikapy@moikaslookout.com>
2024-10-22 18:24:24 -0400

    add nginx settings for staging server

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const { withAxiom } = require('next-axiom');
 /** @type {import('next').NextConfig} */
 const nextConfig = withAxiom({
-  experimental: {
-    typedRoutes: true,
-  },
 });
 
 module.exports = nextConfig;

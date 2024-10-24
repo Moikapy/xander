a375bc663800689ba56ee175cdc5b31361e8a70e
Moikapy <warrenckhan@gmail.com>
2024-10-24 01:00:53 -0400

    Refactor next.config.js to add webpack dev options

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
-const { withAxiom } = require('next-axiom');
+const { withAxiom } = require("next-axiom");
 /** @type {import('next').NextConfig} */
 const nextConfig = withAxiom({
   //output: 'standalone',
-    eslint: {
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
+  },
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000, // Check for changes every 1 second
+        aggregateTimeout: 300, // Delay before rebuilding
+      };
+    }
+    return config;
   },
 });
 

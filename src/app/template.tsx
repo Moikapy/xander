b6b79ba284ed4a669e51ec1005161e27fc290c3f
Warren <moikapy@moikaslookout.com>
2024-11-14 18:28:13 -0500

    Refactor layout component and remove unused code

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -5,17 +5,17 @@ import ChatWidget from '@/components/ChatWidget';
 //import '@/src/styles/globals.css';
 export default function Layout({children}:{children:React.ReactNode}) {
   return (
-    <div className="layout h-dvh flex flex-col font-mono">
+    <div className='layout h-dvh flex flex-col font-mono'>
       <Navbar />
-      <div className="flex flex-col h-full"> {children}</div>
+      {children}
 
       {/* Footer */}
       {/* <ChatWidget /> */}
-      <footer className="w-full flex justify-center py-6 text-sm">
-        <div className="max-w-7xl flex justify-between items-center">
+      <footer className='w-full flex justify-center py-6 text-sm'>
+        <div className='max-w-7xl flex justify-between items-center'>
           <p>
             &copy; {new Date().getFullYear()} Moikas LLC. All Rights Reserved.
-          </p>{" "}
+          </p>{' '}
           <p>v 0.1.0</p>
         </div>
       </footer>
